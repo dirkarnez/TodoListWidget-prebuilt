4c028f2188aa2dbec15811f9e85ba2af41ab5dd8
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-20 20:29:20 +0800

    - update windows.sh

--- windows.sh
+++ windows.sh
@@ -2,14 +2,16 @@
 
 curl -L -O -J https://dl.google.com/android/repository/commandlinetools-win-9477386_latest.zip && \
 7z x commandlinetools-win-9477386_latest.zip -o"/commandlinetools-win-9477386_latest" && \
-\ # export JAVA_HOME="$(cygpath -u D:/Softwares/jdk-11.0.13+8)" && \
+\ 
+# export JAVA_HOME="$(cygpath -u D:/Softwares/jdk-11.0.13+8)" && \
 export ANDROID_HOME="$(cygpath -u $USERPROFILE/Downloads/commandlinetools-win-9477386_latest)" && \
 export PATH="$ANDROID_HOME/cmdline-tools/bin:/usr/local/bin/:/usr/bin/:$JAVA_HOME:$JAVA_HOME/bin" && \
 echo $ANDROID_HOME && \
 echo $JAVA_HOME && \
 sdkmanager.bat --list --sdk_root=$ANDROID_HOME && \
 yes | sdkmanager.bat --sdk_root=$ANDROID_HOME --install "platform-tools" "platforms;android-23" "build-tools;27.0.1" && \
-\ # export JAVA_HOME="$(cygpath -u D:/Softwares/jdk-11.0.13+8)" && \
+\ 
+# export JAVA_HOME="$(cygpath -u D:/Softwares/jdk-11.0.13+8)" && \
 export ANDROID_HOME="$(cygpath -u $USERPROFILE/Downloads/commandlinetools-win-9477386_latest)" && \
 export PATH="$ANDROID_HOME/cmdline-tools/bin:/usr/local/bin/:/usr/bin/:$JAVA_HOME:$JAVA_HOME/bin" && \
 echo $ANDROID_HOME && \
@@ -21,10 +23,10 @@ rm -v -f -r ./key && \
 mkdir ./obj && \
 mkdir ./bin && \
 mkdir ./key && \
-\ # export JAVA_HOME="$(cygpath -u D:/Softwares/jdk8u322-b06)" && \
+\ 
+# export JAVA_HOME="$(cygpath -u D:/Softwares/jdk8u322-b06)" && \
 $ANDROID_HOME/build-tools/27.0.1/aapt package -v -f -m -S ".\res" -J ".\src" -M ./AndroidManifest.xml -I $ANDROID_HOME/platforms/android-23/android.jar && \
 \
-echo "1234" && \
 javac -d ./obj/ -source 1.7 -target 1.7 -classpath $ANDROID_HOME/platforms/android-23/android.jar -sourcepath ./src/*.java ./src/org/chrisbailey/todo/*.java  ./src/org/chrisbailey/todo/activities/*.java ./src/org/chrisbailey/todo/db/*.java  ./src/org/chrisbailey/todo/utils/*.java  ./src/org/chrisbailey/todo/widgets/*.java && \
 $ANDROID_HOME/build-tools/27.0.1/dx.bat --dex --output=".\bin\classes.dex" ".\obj" && \
 $ANDROID_HOME/build-tools/27.0.1/aapt package -f -M ./AndroidManifest.xml -S ".\res" -I $ANDROID_HOME/platforms/android-23/android.jar -F "./bin/HelloWorld.unsigned.apk" ./bin
