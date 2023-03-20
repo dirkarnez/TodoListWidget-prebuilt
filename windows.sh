fe7b739e5f0286094ecca93a25f88919c3689929
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-20 20:40:39 +0800

    - update windows.sh

--- windows.sh
+++ windows.sh
@@ -4,15 +4,15 @@ echo $JAVA_HOME_8_X64 && \
 echo $JAVA_HOME_11_X64 && \
 curl -L -O -J https://dl.google.com/android/repository/commandlinetools-win-9477386_latest.zip && \
 7z x commandlinetools-win-9477386_latest.zip -o"$USERPROFILE/Downloads/commandlinetools-win-9477386_latest" && \
-\ 
-export JAVA_HOME="$(cygpath -u $JAVA_HOME_8_X64)" && \
+\
+export JAVA_HOME="$(cygpath -u $JAVA_HOME_11_X64)" && \
 export ANDROID_HOME="$(cygpath -u $USERPROFILE/Downloads/commandlinetools-win-9477386_latest)" && \
 export PATH="$ANDROID_HOME/cmdline-tools/bin:/usr/local/bin/:/usr/bin/:$JAVA_HOME:$JAVA_HOME/bin" && \
 echo $ANDROID_HOME && \
 echo $JAVA_HOME && \
 sdkmanager.bat --list --sdk_root=$ANDROID_HOME && \
 yes | sdkmanager.bat --sdk_root=$ANDROID_HOME --install "platform-tools" "platforms;android-23" "build-tools;27.0.1" && \
-\ 
+\
 export JAVA_HOME="$(cygpath -u $JAVA_HOME_11_X64)" && \
 export ANDROID_HOME="$(cygpath -u $USERPROFILE/Downloads/commandlinetools-win-9477386_latest)" && \
 export PATH="$ANDROID_HOME/cmdline-tools/bin:/usr/local/bin/:/usr/bin/:$JAVA_HOME:$JAVA_HOME/bin" && \
@@ -25,7 +25,7 @@ rm -v -f -r ./key && \
 mkdir ./obj && \
 mkdir ./bin && \
 mkdir ./key && \
-\ 
+\
 export JAVA_HOME="$(cygpath -u $JAVA_HOME_8_X64)" && \
 $ANDROID_HOME/build-tools/27.0.1/aapt package -v -f -m -S ".\res" -J ".\src" -M ./AndroidManifest.xml -I $ANDROID_HOME/platforms/android-23/android.jar && \
 \
